13428aafb0962f518ed998ed78193c54c0f1f4a9
Kasia <kjaworska@gmail.com>
2019-11-19 21:29:46 +0100

    Dodaj pliki projektów.

---
+++ BasketballQuiz/BasketballQuiz.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "pytanie.h"
+
+using namespace std;
+
+int main()
+{
+	Pytanie p[5];
+	int suma = 0;
+	for (int i = 0; i <= 4; i++)
+	{
+		p[i].nr_pytania = i+1;
+		p[i].wczytaj();
+		p[i].zadaj();
+		p[i].sprawdz();
+		suma += p[i].punkt;
+	}
+
+	cout << "Koniec quizu. Liczba Twoich punktow: " << suma << " pkt/5 pkt." << endl;
+	system("pause");
+	return 0;
+}
